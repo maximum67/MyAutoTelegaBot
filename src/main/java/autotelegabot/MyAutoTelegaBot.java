745c7f08de467504ad566ff52717d7d5fbeccdde
Gigalskiy <gigalskiy@gmail.com>
2021-11-05 17:28:15 +0300

    correction3

--- src/main/java/autotelegabot/MyAutoTelegaBot.java
+++ src/main/java/autotelegabot/MyAutoTelegaBot.java
@@ -1,8 +1,37 @@
 package autotelegabot;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.request.EditMessageText;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.BaseResponse;
+
 public class MyAutoTelegaBot {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        //System.out.println("Hello AutoTelegaBot!");
+
+        TelegramBot bot = new TelegramBot("2131740608:AAFYlF1Ysm8XRaBIkPjpoc3wc7dM8qGVWAA");
+
+
+
+
+        bot.setUpdatesListener(element -> {
+            //System.out.println(element);
+            element.forEach(it -> {
+              //System.out.println(element.size());
+             //bot.execute(new SendMessage(it.message().chat().id(), "Ну-да, привет!"));
+             bot.execute(new SendMessage(it.message().chat().id(), logicBots.BotResponse(it.message().text())));
+
+            });
+
+
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+
+
+
 
     }
 }
