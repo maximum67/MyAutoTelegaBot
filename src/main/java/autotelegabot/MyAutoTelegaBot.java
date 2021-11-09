3af9b2b481e02f0c7e9e5cce3c61f532f08b50c6
67134 <gigalskiy@gmail.com>
2021-11-09 20:32:20 +0300

    commit

--- src/main/java/autotelegabot/MyAutoTelegaBot.java
+++ src/main/java/autotelegabot/MyAutoTelegaBot.java
@@ -2,35 +2,38 @@ package autotelegabot;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
-
 
 public class MyAutoTelegaBot {
 
 
     public static void main(String[] args) {
-        //System.out.println("Hello AutoTelegaBot!");
 
-        TelegramBot bot = new TelegramBot("");
+        BotKey botKeyToken = new BotKey();
 
-
-
+        TelegramBot bot = new TelegramBot(botKeyToken.getKey());
 
         bot.setUpdatesListener(element -> {
-            //System.out.println(element);
+            System.out.println(element);
+
             element.forEach(it -> {
-              //System.out.println(element.size());
-             //bot.execute(new SendMessage(it.message().chat().id(), "Ну-да, привет!"));
-             bot.execute(new SendMessage(it.message().chat().id(), logicBots.BotResponse(it.message().text())));
+                //bot.execute(new SendMessage(it.message().chat().id(),"Здравствуйте, "+it.message().chat().firstName()));
+               SendMessage sendMessage = new SendMessage(it.message().chat().id(), logicBots.BotResponse(it.message().text()));
+                bot.execute(sendMessage);
+
 
             });
 
-
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
+
         });
 
 
 
-
     }
-}
+
+
+ }
+
